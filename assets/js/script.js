cff1a3e91631d5244399cb5a53454d9dbba0e60e
Omar Pacheco <omarspacheco@gmail.com>
2021-09-20 21:31:56 -0400

    added answer functionality, and form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,175 +1,157 @@
 var quizQuestions = [{
         "question": "Which of the following is not a JavaScript data type?",
-        "answers": [{
-                "ansVal": "Decimal",
-                "correctAns": true
-            },
-            {
-                "ansVal": "Boolean",
-                "correctAns": false
-            },
-            {
-                "ansVal": "Object",
-                "correctAns": false
-            },
-            {
-                "ansVal": "Number",
-                "correctAns": false
-            },
-            {
-                "ansVal": "String",
-                "correctAns": false
-            }
-        ]
+        "answers": {
+            "Decimal": true,
+            "Boolean": false,
+            "Object": false,
+            "Number": false,
+            "String": false
+        }
     },
     {
         "question": "Which of the following is a method available from the window object?",
-        "answers": [{
-                "ansVal": "prompt()",
-                "correctAns": true
-            },
-            {
-                "ansVal": "maximize()",
-                "correctAns": false
-            },
-            {
-                "ansVal": "attribute()",
-                "correctAns": false
-            },
-            {
-                "ansVal": "record()",
-                "correctAns": false
-            },
-            {
-                "ansVal": "playback()",
-                "correctAns": false
-            }
-        ]
+        "answers": {
+            "prompt()": true,
+            "maximize()": false,
+            "attribute()": false,
+            "record()": false,
+            "playback()": false
+        }
     },
     {
         "question": "Which of the following browser methods can be used to start a timer in JavaScript?",
-        "answers": [{
-                "ansVal": "setInterval()",
-                "correctAns": true
-            },
-            {
-                "ansVal": "setTimer()",
-                "correctAns": false
-            },
-            {
-                "ansVal": "startTimer()",
-                "correctAns": false
-            },
-            {
-                "ansVal": "startInterval()",
-                "correctAns": false
-            },
-            {
-                "ansVal": "createTimer()",
-                "correctAns": false
-            }
-        ]
+        "answers": {
+            "setInterval()": true,
+            "setTimer()": false,
+            "startTimer()": false,
+            "startInterval()": false,
+            "createTimer()": false
+        }
     },
     {
         "question": "Which of the following can be used to comment out a line in JavaScript?",
-        "answers": [{
-                "ansVal": "// This is a comment",
-                "correctAns": true
-            },
-            {
-                "ansVal": "*/ This is a comment",
-                "correctAns": false
-            },
-            {
-                "ansVal": "/ This is a comment",
-                "correctAns": false
-            },
-            {
-                "ansVal": "% This is a comment",
-                "correctAns": false
-            }
-        ]
+        "answers": {
+            "// This is a comment": true,
+            "*/ This is a comment": false,
+            "/ This is a comment": false,
+            "% This is a comment": false
+        }
     },
     {
         "question": "If <span class=\"code-snippet\">var a = 10</span> and <span class=\"code-snippet\">var b = '10'</span> which of the following would return true?",
-        "answers": [{
-                "ansVal": "a == b",
-                "correctAns": true
-            },
-            {
-                "ansVal": "a = b",
-                "correctAns": false
-            },
-            {
-                "ansVal": "a === b",
-                "correctAns": false
-            },
-            {
-                "ansVal": "a equals b",
-                "correctAns": false
-            }
-        ]
+        "answers": {
+            "a == b": true,
+            "a = b": false,
+            "a === b": false,
+            "a equals b": false
+        }
     },
     {
         "question": "Does JavaScript support automatic type conversion?",
-        "answers": [{
-                "ansVal": "Yes",
-                "correctAns": true
-            },
-            {
-                "ansVal": "No",
-                "correctAns": false
-            }
-        ]
+        "answers": {
+            "Yes": true,
+            "No": false
+        }
     },
     {
         "question": "What method can be used to convert a string to a number?",
-        "answers": [{
-                "ansVal": "parseInt()",
-                "correctAns": true
-            },
-            {
-                "ansVal": "parseNumber()",
-                "correctAns": false
-            },
-            {
-                "ansVal": "toNumber()",
-                "correctAns": false
-            },
-            {
-                "ansVal": "toInt()",
-                "correctAns": false
-            }
-        ]
+        "answers": {
+            "parseInt()": true,
+            "parseNumber()": false,
+            "toNumber()": false,
+            "toInt()": false
+        }
     }
 ];
 var timer;
-var timerVal = 120;
+var timerVal;
 var questionCounter = 0;
+var score;
 
 var timerEl = document.querySelector(".quiz-timer");
 var quizQuestionEl = document.querySelector(".quiz-question");
 var quizAnswersEl = document.querySelector(".quiz-answers");
-var getStartedButton = document.querySelector(".quiz-content button");
+var getStartedButton = document.querySelector(".get-started-button");
 var quizCardEl = document.querySelector(".quiz-card");
 var welcomeTextEl = document.querySelector(".quiz-welcome-text");
+var displayedAnsw = document.querySelector(".answer-list")
+var answerSection = document.querySelector(".answer-section")
+var resultEl = document.querySelector(".ans-result");
+var scoreForm = document.querySelector(".submit-score");
 
 timerEl.textContent = timerVal;
-console.log(quizQuestions);
 
-getStartedButton.addEventListener("click", function () {
+getStartedButton.addEventListener("click", function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    timerVal = 120;
+    score = 0;
     timerEl.textContent = timerVal;
-    quizQuestions = shuffleArray(quizQuestions);
     displayWelcomeFormat(false);
+    quizQuestions = shuffleArray(quizQuestions);
     switchCard();
     timer = setInterval(function () {
         timerVal--;
         timerEl.textContent = timerVal;
         if (timerVal === 0) {
             clearInterval(timer);
+            showQuizEnd(true);
         }
     }, 1000)
 });
+
+displayedAnsw.addEventListener("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var element = event.target;
+    if (element.matches(".answer-button")) {
+        checkCorrectAnsw(element.textContent);
+    }
+});
+
+scoreForm.addEventListener("submit", function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    console.log("Submitted!");
+    quizQuestionEl.textContent = "Thank you!"
+});
+
+function checkCorrectAnsw(answText) {
+    var currAnswers = quizQuestions[questionCounter].answers;
+    var correctAns = currAnswers[answText];
+    if (correctAns) {
+        resultEl.querySelector('p').textContent = "Correct!";
+    } else {
+        resultEl.querySelector('p').textContent = "Wrong!";
+    }
+    resultEl.classList.remove("hidden");
+    var secPassed = 0;
+    var resultTimer = setInterval(function () {
+        if (secPassed === 5) {
+            resultEl.classList.add("hidden");
+            clearInterval(resultTimer);
+        }
+        secPassed++;
+    }, 1000)
+    questionCounter++;
+    if (questionCounter === quizQuestions.length) {
+        showQuizEnd(false);
+    } else {
+        switchCard();
+    }
+}
+
+function showQuizEnd(timedOut) {
+    if (timedOut) {
+        quizQuestionEl.textContent = "Oh man, you ran out of time!";
+    } else {
+        clearInterval(timer);
+        quizQuestionEl.textContent = "Great job, you finished!";
+    }
+    timerEl.classList.add('hidden')
+    scoreForm.classList.remove('hidden');
+    displayedAnsw.classList.add('hidden');
+}
 
 function displayWelcomeFormat(shouldDisplay) {
     if (shouldDisplay) {
@@ -190,51 +172,23 @@ function switchCard() {
 
 function displayAnswers(answers) {
     answers = shuffleArray(answers);
-    var displayedAnsw = document.querySelector(".answer-list")
     var answerlist = document.createElement("ul");
     answerlist.className = "answer-list";
     // var answerHtmlString = '<ul class="answer-list">'
-    answers.forEach((currAnswer, index) => {
+    Object.keys(answers).forEach((key, index) => {
         var answId = 'answ' + index;
         var answItem = document.createElement("li");
         answItem.id = answId;
         answItem.className = "answer-item";
         var answButton = document.createElement("button");
         answButton.className = "answer-button";
-        answButton.textContent = currAnswer.ansVal;
+        answButton.textContent = key;
         answItem.appendChild(answButton);
         answerlist.appendChild(answItem);
-        displayedAnsw.addEventListener("click", addClickToAnswer(event, currAnswer.correctAns))
+        console.log(answButton);
     });
     displayedAnsw.innerHTML = answerlist.innerHTML;
 }
-
-function addClickToAnswer(event, correctAns) {
-    event.preventDefault();
-    event.stopPropagation();
-    var element = event.target;
-    console.log("Registered Click");
-    if (element.matches(".answer-item")) {
-        console.log("Registered Click");
-        var resultEl = document.querySelector(".ans-result p");
-        if (correctAns) {
-            resultEl.textContent = "Correct!";
-        } else {
-            resultEl.textContent = "Wrong!";
-        }
-        resultEl.classList.remove("hidden");
-        var resultTimer = setInterval(function () {
-            if (resultTimer === 2) {
-                resultEl.classList.add("hidden");
-                clearInterval(resultTimer);
-            }
-        }, 1000)
-    }
-}
-
-// function displayHighScore() {
-//     quizQuestion.
-// }
 
 function shuffleArray(array) {
     var cntr = array.length;
